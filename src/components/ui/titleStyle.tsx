f2e9d6afb69615d236598b540fbba165ae551d56
Aminul721 <aminul721@gmail.com>
2025-07-16 23:40:21 +0600

    feat: 1 bug fix

--- src/components/ui/titleStyle.tsx
+++ src/components/ui/titleStyle.tsx
@@ -1,6 +1,11 @@
 import React from "react";
-
-const TitleStyle = ({title, desc}) => {
+interface PropsType {
+  id?: number | string;
+  img?: string | any;
+  title?: string;
+  desc?: string;
+}
+const TitleStyle:React.FC<PropsType> = ({title, desc}) => {
     return (
         <div className='text-center max-w-[50rem] mx-auto mb-8'>
             <h2 className='text-h2 mb-4 lg:mb-5'>{title}</h2>
